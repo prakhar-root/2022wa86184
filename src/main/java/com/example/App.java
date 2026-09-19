14e0d627e5bf8425f9343a698e4eff907479a2a2
prakhar-root <prakharmishra16021999@gmail.com>
2026-09-19 22:26:43 +0530

    Fix CI source code

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("System.out.println("Hello from BITS DevOps Lab - Continuous Integration");
+        System.out.println("Hello from BITS DevOps Lab - Continuous Integration");
     }
-
+}
